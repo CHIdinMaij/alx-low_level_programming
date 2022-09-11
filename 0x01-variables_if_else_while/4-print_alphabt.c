41efa1c8e86cd8e25d25b04c420d2ee8c0aa3ad4
Chidinma <cijeoma214@gmail.com>
2022-09-10 19:02:48 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,20 +3,22 @@
 #include <time.h>
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char low, e, q;
+
 	e = 'e';
 	q = 'q';
 
 	for (low = 'a'; low <= 'z'; low++)
 	{
 		if (low != e && low != q)
-			putchar(low);
+                	putchar(low);
 	}
-	printf("\n");
+	        printf("\n");
 	
 	return (0);
 
