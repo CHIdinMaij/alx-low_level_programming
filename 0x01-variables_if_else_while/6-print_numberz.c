d956c7534639767269898778eb6ae1d745849b81
Chidinma <cijeoma214@gmail.com>
2022-09-10 18:46:38 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	        putchar('\n');
+
+		return (0);
+}
+
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main 
+{
+	int d;
+	
+	for (d = '0'; d <='9'; d++)
+	{
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',');
+			putchar(',');
+		}
+
+	}
+	putchar('\n');
+
+	return (0);
+}
