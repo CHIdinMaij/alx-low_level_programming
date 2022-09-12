8035d12526ceca9dc0a9c1b87f4f79ad540dbac4
Chidinma <cijeoma214@gmail.com>
2022-09-12 13:22:31 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,28 +5,25 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- *
  */
-
 int main(void)
 {
-
 	int d;
 
-	for (d = '0'; d <= '9'; d++)
-
+	for (d = 0; d < 90; d++)
 	{
-	putchar(d);
-	if (d != '9')
-
+		for ()
 		{
-	putchar(',');
-	putchar(',');
+			putchar((d / 10) + '0');
+			putchar((d % 10) + '0');
 		}
+		if (d != 89)
+		{
 
+			putchar(',');
+			putchar(',');
+		}
 	}
 	putchar('\n');
 	return (0);
-
 }
-
